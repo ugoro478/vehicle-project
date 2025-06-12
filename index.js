6587dee086a24b6ced458d882287753137ab9cb1
Roland <Ugoroland99@gmail.com>
2025-06-12 11:16:50 +0100

    up

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 "use strict";
 // Implement class
 class Car {
-    constructor(make, model, year) {
-        this.make = make;
-        this.model = model;
-        this.year = year;
-    }
-    start() {
-        console.log("Car engine started");
-    }
+  constructor(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+  start() {
+    console.log("Car engine started");
+  }
 }
 // Create an instance and call start
 const myCar = new Car("Toyota", "Corolla", 2022);
